510aa745344296b7e346ef533bc1b564e055f659
emir-munoz <emir@emunoz.org>
2013-03-04 11:50:59 +0000

    Updates wikitable

--- publish.sh
+++ publish.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
-#GIT_DIR_="$(git rev-parse --git-dir)"
+# Push files @Emir Muñoz
 BRANCH="$(git rev-parse --symbolic --abbrev-ref $(git symbolic-ref HEAD))"
+
 ADD="$(git add -A .)"
 test -x "$ADD" && exec "$ADD" "$@"
+
 COMMIT="$(git commit -m 'Updates wikitable')"
 test -x "$COMMIT" && exec "$COMMIT" "$@"
 
-git push origin "$BRANCH"
-
-#PRE_PUSH="$GIT_DIR_/hooks/pre-push"
-#POST_PUSH="$GIT_DIR_/hooks/post-push"
-
-#test -x "$PRE_PUSH" &&
-#    exec "$PRE_PUSH" "$BRANCH" "$@"
-
-#git push "$@"
-
-#test $? -eq 0 && test -x "$POST_PUSH" &&
-#    exec "$POST_PUSH" "$BRANCH" "$@"
+PUSH="$(git push origin $BRANCH)"
+test -x "PUSH" && exec "$PUSH" "$@"
